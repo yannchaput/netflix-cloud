1eef98921d1c3a4362313cb7b5f20f3bb796ba32
yannchaput <yannchaput@gmail.com>
2016-11-06 18:57:38 +0100

    ajout de la strusture de l'api cliente

--- customer-api/src/main/java/com/innovention/hystrix/api/customers/CustomerApiApplication.java
+++ customer-api/src/main/java/com/innovention/hystrix/api/customers/CustomerApiApplication.java
@@ -7,13 +7,14 @@ import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class CustomerApiApplication {
 	
-	@RequestMapping(value="/customers")
+	@RequestMapping(value="/customers", method=RequestMethod.GET)
 	public List<Customer> customers() {
 		
 		ArrayList<Customer> list = new ArrayList<>();
